0235b7d2dcdf2cce6e45930e080721ce2de718d1
JacobLinCool <jacoblincool@gmail.com>
2018-10-28 12:08:55 +0800

    1

---
+++ lib.js
@@ -0,0 +1,43 @@
+// Javascript Document
+
+var XhrPlugin = document.createElement("script");
+XhrPlugin.src = "https://cdn.jsdelivr.net/gh/Merret/UsefulJS@0.4.2/js/xhr.min.js";
+document.body.appendChild(XhrPlugin);
+
+var MDB = (function () {
+	var create = function (Data) {
+		var casper = JSON.parse(xhr.post("https://api.myjson.com/bins", JSON.stringify(Data)));
+		return casper.uri.substring(28);
+	};
+	var connect = function (DBcode) {
+		var casper = JSON.parse(xhr.get("https://api.myjson.com/bins/" + DBcode));
+		return casper;
+	};
+	var edit = function (DBcode, Data) {
+		var casper = JSON.parse(xhr.put("https://api.myjson.com/bins/" + DBcode, JSON.stringify(Data)));
+		if (casper == Data) {
+			return {
+				status: "success",
+				code: 1,
+				data: casper
+			}
+		} else {
+			return {
+				status: "failed",
+				code: 0
+			}
+		}
+	};
+	var search = function (DBcode, Index="all", Data="all") {
+		var casper = JSON.parse(xhr.get("https://api.myjson.com/bins/" + DBcode));
+		
+		return;
+	};
+
+	return {
+		create: create,
+		connect: connect,
+		edit: edit,
+		search: search
+	};
+})();
